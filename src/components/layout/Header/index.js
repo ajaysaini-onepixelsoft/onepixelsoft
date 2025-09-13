a4fba927332638ac7acf9833304baa6a5e4d442f
ajaysaini-onepixelsoft <onepixelsoft.ajaysaini@gmail.com>
2025-09-13 12:51:33 +0530

    add new logo

--- src/components/layout/Header/index.js
+++ src/components/layout/Header/index.js
@@ -13,42 +13,42 @@ import Link from 'next/link';
 
 
 export default function Header() {
-  const [isOpen, setIsOpen] = useState(null);
-  const [isMobile, setIsMobile] = useState(false);
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [istopset, setIsTopSet] = useState(false)
-  useEffect(() => {
-    const changed = () => setIsMobile(window.innerWidth <= 992)
-    changed()
-    window.addEventListener('resize', changed)
-    return () => window.removeEventListener('resize', changed)
-  }, []);
+    const [isOpen, setIsOpen] = useState(null);
+    const [isMobile, setIsMobile] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [istopset, setIsTopSet] = useState(false)
+    useEffect(() => {
+        const changed = () => setIsMobile(window.innerWidth <= 992)
+        changed()
+        window.addEventListener('resize', changed)
+        return () => window.removeEventListener('resize', changed)
+    }, []);
 
-  useEffect(() => {
-    const fixedTop = document.querySelector(".header-area");
-    if (!fixedTop) return;
+    useEffect(() => {
+        const fixedTop = document.querySelector(".header-area");
+        if (!fixedTop) return;
 
-    const onScroll = () => {
-      if (window.scrollY > 50) {
-        setIsTopSet(true)
-        fixedTop.classList.add("menu-fixed", "animated", "fadeInDown");
-      } else {
-        setIsTopSet(false)
-        fixedTop.classList.remove("menu-fixed", "fadeInDown");
-      }
-    };
+        const onScroll = () => {
+            if (window.scrollY > 50) {
+                setIsTopSet(true)
+                fixedTop.classList.add("menu-fixed", "animated", "fadeInDown");
+            } else {
+                setIsTopSet(false)
+                fixedTop.classList.remove("menu-fixed", "fadeInDown");
+            }
+        };
 
-    window.addEventListener("scroll", onScroll);
-    return () => window.removeEventListener("scroll", onScroll);
-  }, []);
+        window.addEventListener("scroll", onScroll);
+        return () => window.removeEventListener("scroll", onScroll);
+    }, []);
 
-  return (
+    return (
 
-    <section className='header-area header-main sticky z-50'>
-      <div className='header bg-[#000000] text-white xl:px-40 lg:px-10 px-5 py-2 text-center flex justify-between items-center'>
+        <section className='header-area header-main sticky z-50'>
+      <div className='header bg-[#000000] text-white xl:px-40 lg:px-10 px-5 py-1 text-center flex justify-between items-center'>
         <div className="logo animate__animated_shakeX text-3xl font-bold bg-white  px-3 py-1 rounded-md relative animate__animated animate__fadeIn hover:animate__shakeX ">
           <Link href="/">
-            <Image src="/assets/images/logo/onepixel-logo.png" alt="OnePixelSoft Logo" className=" bg-white rounded-md" width={150} height={100} loading="lazy" />
+            <Image src="/assets/images/logo/onepixel-logo.png" alt="OnePixelSoft Logo" className=" bg-white rounded-md" width={200} height={150} loading="lazy" />
           </Link>
         </div>
 
@@ -165,5 +165,5 @@ export default function Header() {
         }
       </div>
     </section>
-  )
+    )
 }
